4712dedc0713d80ff4865939cdd6e59e2b370e37
sina <ebr.sina@gmail.com>
2018-12-29 02:30:49 +0330

    end 123

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,13 +13,20 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null,title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
 
 exports.postEditProduct = (req, res, next) => {
-  
+  const prodId = req.body.prodId;
+  const upTitle = req.body.title;
+  const upPrice = req.body.price;
+  const upImg = req.body.imageUrl;
+  const upDesc = req.body.description;
+  const upProd = new Product(prodId, upTitle, upImg, upDesc, upPrice);
+  upProd.save();
+  res.redirect('/admin/products');
 } 
 
 exports.getEditProduct = (req, res, next) => {
